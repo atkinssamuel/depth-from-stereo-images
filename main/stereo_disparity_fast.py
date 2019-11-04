0d3631c12a7c3d2e1b4b68a436e2fa0104b3b0f2
Samuel Atkins <s.m.atkins73@gmail.com>
2019-11-04 12:08:39 -0500

    Created naive image correspondence algorithm

--- main/stereo_disparity_fast.py
+++ main/stereo_disparity_fast.py
@@ -2,6 +2,30 @@ import numpy as np
 from numpy.linalg import inv
 from scipy.ndimage.filters import *
 
+
+def sum_of_absolute_differences(matrix_a, matrix_b):
+    difference = abs(matrix_a - matrix_b).flatten()
+    SAD = sum(difference)
+    return SAD
+
+
+def window(image, x, y, d):
+    left = x - d if x - d > 0 else 0
+    right = x + d if x + d < image.shape[0] else image.shape[0]
+    top = y - d if y - d > 0 else 0
+    bottom = y + d if y + d < image.shape[1] else image.shape[1]
+    return image[top:bottom, left:right]
+
+
+def window_bounds(Il, Ir, x, y, d):
+    left = x - d if x - d > 0 else 0
+    right = min(x + d, Il.shape[1], Ir.shape[1])
+    top = y - d if y - d > 0 else 0
+    bottom = min(y + d, Il.shape[0], Ir.shape[0])
+    return [left, right, top, bottom]
+
+
+
 def stereo_disparity_fast(Il, Ir, bbox, maxd):
     """
     Fast stereo correspondence algorithm.
@@ -35,10 +59,40 @@ def stereo_disparity_fast(Il, Ir, bbox, maxd):
     #
     #  - Don't optimize for runtime (too much), optimize for clarity.
 
-    #--- FILL ME IN ---
+    # --- FILL ME IN ---
 
     # Your code goes here.
-    
-    #------------------
 
+    # ------------------
+    # Max disparity value = 63 pixels
+    UL = np.array([bbox[0][0], bbox[1][0]])
+    UR = np.array([bbox[0][1], bbox[1][0]])
+    BR = np.array([bbox[0][1], bbox[1][1]])
+    BL = np.array([bbox[0][0], bbox[1][1]])
+
+    left = bbox[0][0]
+    right = bbox[0][1]
+    up = bbox[1][0]
+    down = bbox[1][1]
+
+    window_size = 5
+    Id = np.zeros((Il.shape[0], Il.shape[1]))
+    for y in range(up, down):
+        for x in range(left, right):
+            disparity_list = []
+            for disparity in range(-maxd, maxd):
+                # Define windows of equal size in left and right images:
+                [L, R, T, B] = window_bounds(Il, Ir, y, x - disparity, window_size)
+                Il_window = Il[T:B, L:R]
+                Ir_window = Ir[T:B, L:R]
+
+                # Compute the SAD score between the windows:
+                SAD = sum_of_absolute_differences(Il_window, Ir_window)
+
+                # Add score to list to be sorted later:
+                disparity_list.append([SAD, disparity])
+
+            # Choose the disparity with the lowest SAD score
+            disparity_list = np.sort(np.array(disparity_list), axis=0)
+            Id[y, x] = disparity_list[0][1]
     return Id
--- test/fast_test.py
+++ test/fast_test.py
@@ -1,7 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import  matplotlib.pyplot as plt
 from mat4py import loadmat
 from imageio import imread
+from matplotlib import patches
+
 from main.stereo_disparity_fast import stereo_disparity_fast
 
 Il = imread("../images/teddy_image_02.png", as_gray = True)
@@ -11,6 +14,33 @@ Ir = imread("../images/teddy_image_06.png", as_gray = True)
 bboxes = loadmat("../images/bboxes.mat")
 bbox = np.array(bboxes["teddy_02"]["bbox"])
 
+# Setting the UL, UR, BR, and BL corners from the bounds array:
+UL = np.array([bbox[0][0], bbox[1][0]])
+UR = np.array([bbox[0][1], bbox[1][0]])
+BR = np.array([bbox[0][1], bbox[1][1]])
+BL = np.array([bbox[0][0], bbox[1][1]])
+
+bounds_list = UL, UR, BR, BL
+bounding_poly = [UL, UR, BR, BL, UL]
+
+# Defining inner and outer borders using matplotlib's Path:
+codes = [
+    Path.MOVETO,
+    Path.LINETO,
+    Path.LINETO,
+    Path.LINETO,
+    Path.CLOSEPOLY
+]
+path = Path(bounding_poly, codes)
+
+patch = patches.PathPatch(path, facecolor="none", lw=2)
+
+fig, ax = plt.subplots()
+ax.add_patch(patch)
+plt.imshow(Il, cmap="gray")
+plt.show()
 Id = stereo_disparity_fast(Il, Ir, bbox, 52)
-plt.imshow(Id, cmap = "gray")
+
+plt.imshow(Id, cmap="gray")
+plt.savefig('../results/Id')
 plt.show()
